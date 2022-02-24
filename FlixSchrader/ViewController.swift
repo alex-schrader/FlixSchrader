fda6d5c162e8c573992d78bee5c9738e528981ac
aschrad1 <aschrad1@swarthmore.edu>
2022-02-24 18:14:37 -0500

    Initial Commit

---
+++ FlixSchrader/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FlixSchrader
+//
+//  Created by Alex Schrader on 2022/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
