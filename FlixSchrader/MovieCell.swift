77e3ef2da803308b54191d666c4fef27c2247bc5
aschrad1 <aschrad1@swarthmore.edu>
2022-02-24 20:16:36 -0500

    scrolling list of movies

---
+++ FlixSchrader/MovieCell.swift
@@ -0,0 +1,27 @@
+//
+//  MovieCell.swift
+//  FlixSchrader
+//
+//  Created by Alex Schrader on 2022/02/24.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    @IBOutlet weak var posterView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Pods/Target Support Files/Pods-FlixSchrader/Pods-FlixSchrader-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FlixSchrader : NSObject
+@end
+@implementation PodsDummy_Pods_FlixSchrader
+@end
