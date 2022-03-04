2cc5d43b76b78b04674c396e69255d16a0d03925
aschrad1 <aschrad1@swarthmore.edu>
2022-03-04 13:05:44 -0500

    part 2

---
+++ FlixSchrader/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  FlixSchrader
+//
+//  Created by Alex Schrader on 2022/03/04.
+//
+
+import UIKit
+
+
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
